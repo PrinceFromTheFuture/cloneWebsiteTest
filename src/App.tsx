a42dbde50619891ca33cbd7e4cbfc60ad8685eda
Amir Waisblay <amirwais.projects@gmail.com>
2024-06-28 11:55:32 +0300

    will complete project set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>fdfd</div>;
-}
+const App = () => {
+  return <div className=" bg-lightGray w-full"></div>;
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#222",
+      lightGray: "#f8f8f8",
+      mainBg: "#a1d3c3",
+      btnBg: "#383838",
+    },
+    extend: {
+      fontFamily: {
+        apercu: ["Apercu", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
